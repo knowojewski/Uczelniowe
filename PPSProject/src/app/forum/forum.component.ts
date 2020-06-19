9f0afe4d7e63a6a0119183d4ad60534731016ec7
knowojewski <xsardas.geza@gmail.com>
2020-06-19 19:39:03 +0200

    Forum component added

--- PPSProject/src/app/forum/forum.component.ts
+++ PPSProject/src/app/forum/forum.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { faUsers, faUser, faComment, faComments } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-forum',
@@ -6,10 +7,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./forum.component.css']
 })
 export class ForumComponent implements OnInit {
+  faUsers = faUsers;
+  faUser = faUser;
+  faComment = faComment;
+  faComments = faComments;
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  scrollToThreads() {
+    const threads = document.querySelector('.threads-section');
+    threads.scrollIntoView({behavior: "smooth"});
   }
 
 }
